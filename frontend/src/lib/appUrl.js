57abbeff36e86ecb8a2395efa4921c0690916f8f
raffaelrenga84-code <raffael.renga84@gmail.com>
2026-07-19 15:59:30 +0200

    Create appUrl.js

---
+++ frontend/src/lib/appUrl.js
@@ -0,0 +1,7 @@
+/**
+ * URL canonico di FAMMY per tutti i link GENERATI (inviti, QR, feed
+ * calendario, condivisioni). Migrazione dominio 18/07/2026:
+ * farxer.com resta attivo per le PWA gia' installate (stesso deploy,
+ * stesso database), ma tutto cio' che esce dall'app punta al nuovo.
+ */
+export const APP_URL = 'https://www.myfammy.app';
